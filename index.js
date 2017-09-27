b8777057508abd46fc007197d1a7c0e510e3f2dd
Tim Johnson <timpjohnson818@gmail.com>
2017-09-27 20:06:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function whisper(string){return string.toLowerCase()}
 function logShout(string){
   console.log('HELLO')}
 
-function logWhisper(string){console.log(string.toLowerCase)}
+function logWhisper(string){console.log('hello')}
 
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
