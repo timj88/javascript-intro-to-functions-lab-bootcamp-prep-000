d9d1815423b40ee9b38ee2443007035271a0b87c
Tim Johnson <timpjohnson818@gmail.com>
2017-09-27 20:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ function whisper(string){return string.toLowerCase()}
 function logShout(string){
   console.log('HELLO')}
 
-function logWhisper(string){console.log('hello')}
-function sayHiToGrandma(string) {
- if string.toUpperCase() === string) {
-     return "YES INDEED!"  }
- else if (string === "I love you, Grandma.") {
- return "I love you, too."
- }
- else {
- return ("I can't hear you!")
+  function sayHiToGrandma(string) {
+    if (string.toUpperCase() === string) {
+      return "YES INDEED!"
+    }
+    else if (string === "I love you, Grandma.") {
+      return "I love you, too."
+    }
+    else {
+      return ("I can't hear you!")
+  }
+  }
